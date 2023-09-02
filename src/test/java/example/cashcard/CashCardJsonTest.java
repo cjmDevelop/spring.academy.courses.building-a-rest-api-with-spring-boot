04467c0187fe81bb5e6478e70c250c5e177ca2d7
cjmDev <manbodhclaude@yahoo.com>
2023-09-02 13:40:37 -0400

    initial fail for de-sererialization method, expected wrong values

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -29,6 +29,21 @@ public class CashCardJsonTest {
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
                 .isEqualTo(123.45);
     }
+
+    @Test//fails due to incorrect expected values
+    public void cashCardDeserializationTest() throws IOException {
+        String expected = """
+           {
+               "id":99,
+               "amount":123.45
+           }
+           """;
+        assertThat(json.parse(expected))
+                .isEqualTo(new CashCard(1000L, 67.89));
+        assertThat(json.parseObject(expected).id()).isEqualTo(1000);
+        assertThat(json.parseObject(expected).amount()).isEqualTo(67.89);
+    }
+
 }
-    
+
 
