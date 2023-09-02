26b83de425c4d4ff3421428ae8667dcf8d790ade
cjmDev <manbodhclaude@yahoo.com>
2023-09-02 12:24:07 -0400

    example test, written to fail on purpose; Now in the red-zone of the Red, Green, Refactor Loop.

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,4 +1,19 @@
 package example.cashcard;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CashCardJsonTest {
+
+   //TDD: Initial Test, written to fail on purpose; based on the Red, Green, Refactor Loop.
+    @Test
+    public void myFirstTest() {
+        assertThat(1).isEqualTo(42);
+    }
 }
+   /*The Red, Green, Refactor Loop
+    - Red zone is when we write a failing test for the desired functionality.
+    - Green zone is when we implement the simplest and most concise solution to make the test pass.
+    - Refactor / look for opportunities to further reduce / simplify, and make the code more efficient
+      by reducing unneeded code; repeat the cycle.*/
