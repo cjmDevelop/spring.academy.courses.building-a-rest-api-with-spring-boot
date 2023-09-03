abb7b0712ac5d2227a342f4099b701049aa487b3
cjmDev <manbodhclaude@yahoo.com>
2023-09-02 22:17:13 -0400

    false positive pass for get test method, not properly checking for id value

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,5 +1,7 @@
 package example.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,18 +16,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
 
-	@Autowired//Note that while @Autowired is a form of Spring dependency injection it’s best used only in tests.
-	TestRestTemplate request;
+    @Autowired//Note that while @Autowired is a form of Spring dependency injection it’s best used only in tests.
+    TestRestTemplate request;
 
-	@Test
-	void shouldReturnACashCardWhenDataIsSaved(){
-	ResponseEntity<String> response = request.getForEntity("/cashcards/99", String.class);
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = request.getForEntity("/cashcards/99", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-	assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-	}
+		//DocumentContext converts the response String into a JSON-aware object with lots of helper methods.
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
 
-	@Test
-	void contextLoads() {
-	}
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
