5a55efadb62262e9163752f4e4d966a650c488cc
cjmDev <manbodhclaude@yahoo.com>
2023-09-02 23:20:15 -0400

    added get test method, updated controller class, added @PathVariable, all test are passing

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -34,7 +34,11 @@ class CashCardApplicationTests {
     }
 
     @Test
-    void contextLoads() {
+    void shouldNotReturnACashCardWithUnknownId() {
+        ResponseEntity<String> response =  request.getForEntity("/cashcards/1000", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
     }
 
 }
