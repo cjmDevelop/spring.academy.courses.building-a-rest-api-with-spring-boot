93e85c84d983de05e280dd7d2e2c07b57fe58538
cjmDev <manbodhclaude@yahoo.com>
2023-09-11 23:13:40 -0400

    Mix up due to tutorial refactoring test json file (expected.json), changed it to single.json

--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -21,7 +21,7 @@ public class CashCardJsonTest {
     @Test//Green Zone: all related files updated for test to now pass.
     public void cashCardSerializationTest() throws IOException {
         CashCard cashCard = new CashCard(99L, 123.45);
-        assertThat(json.write(cashCard)).isStrictlyEqualToJson("expected.json");
+        assertThat(json.write(cashCard)).isStrictlyEqualToJson("single.json");
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.id");
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
                 .isEqualTo(99);
